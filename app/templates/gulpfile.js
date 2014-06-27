0ac3e9390a9ca7c3b9cdfcc5da9b144ab96a95dc
Alex Luke <alexl@bnj.com>
2014-06-27 10:17:25 -0700

    Live reload

--- app/templates/gulpfile.js
+++ app/templates/gulpfile.js
@@ -2,9 +2,11 @@ var gulp = require('gulp');
 
 var inlinesource = require('gulp-inline-source');
 var inlineCss = require('gulp-inline-css');
+var connect = require('gulp-connect');
 
 var paths = {
   html: '*.html',
+  styles: 'styles/*.css',
   dist: 'build',
 };
 
@@ -16,4 +18,21 @@ gulp.task('build', function() {
     }))
     .pipe(gulp.dest(paths.dist));
 });
+
+gulp.task('connect', function() {
+  connect.server({
+    livereload: true,
+  });
+});
+
+gulp.task('reload', function() {
+  gulp.src(paths.html)
+    .pipe(connect.reload());
+});
+
+gulp.task('watch', function() {
+  gulp.watch([paths.html, paths.styles], ['reload']);
+});
+
+gulp.task('default', ['connect', 'watch']);
 
