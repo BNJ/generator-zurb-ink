9aa7942e954309acad7ff314437d64cf8faeb255
Alex Luke <alexl@bnj.com>
2014-06-30 15:27:29 -0700

    Minify images

--- app/templates/gulpfile.js
+++ app/templates/gulpfile.js
@@ -3,20 +3,31 @@ var gulp = require('gulp');
 var inlinesource = require('gulp-inline-source');
 var inlineCss = require('gulp-inline-css');
 var connect = require('gulp-connect');
+var imagemin = require('gulp-imagemin');
+var pngcrush = require('imagemin-pngcrush');
 
 var paths = {
   html: '*.html',
   styles: 'styles/*.css',
-  dist: 'build',
+  images: 'images/*',
+  dist: 'build/',
 };
 
-gulp.task('build', function() {
+gulp.task('inline', function() {
   return gulp.src(paths.html)
     .pipe(inlinesource())
     .pipe(inlineCss({
       preserveMediaQueries: true,
     }))
     .pipe(gulp.dest(paths.dist));
+});
+
+gulp.task('imagemin', function() {
+  gulp.src(paths.images)
+    .pipe(imagemin({
+      use: [pngcrush()]
+    }))
+    .pipe(gulp.dest(paths.dist + 'images'));
 });
 
 gulp.task('connect', function() {
@@ -33,6 +44,10 @@ gulp.task('reload', function() {
 gulp.task('watch', function() {
   gulp.watch([paths.html, paths.styles], ['reload']);
 });
+
+gulp.task('clean', require('del').bind(null, [paths.dist]));
+
+gulp.task('build', ['inline', 'imagemin']);
 
 gulp.task('default', ['connect', 'watch']);
 
